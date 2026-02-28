20874c3c278094da0809b0d3b576f4f3908aee12
sodmaq <sodmaq@gmail.com>
2026-02-28 17:54:58 +0100

    custom button

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,13 @@
+import { TextInput, View } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
-  return <div></div>;
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
 
 export default StartGameScreen;
