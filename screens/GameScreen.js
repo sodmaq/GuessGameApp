32c01bc6cad89f7aac41f1dded2c637419c1919f
sodmaq <sodmaq@gmail.com>
2026-03-05 18:33:23 +0100

    displaying guess

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,28 @@
 import { Text, View, StyleSheet } from 'react-native';
 
-import Title from '../components/Title';
-function GameScreen() {
+import Title from '../components/ui/Title';
+import { useState } from 'react';
+import NumberContainer from '../components/game/NumberContainer';
+
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  }
+
+  return rndNum;
+}
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       {/* Guess */}
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+
       <View>
         <Text>Game Screen</Text>
       </View>
